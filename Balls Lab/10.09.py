b9a16f0a0aecb7931dd8f8039a8a615b82c8f9ca
MechHedgehog <71868148+MechHedgehog@users.noreply.github.com>
2020-11-07 22:12:22 +0300

    For issue

--- Balls Lab/10.09.py
+++ Balls Lab/10.09.py
@@ -9,9 +9,17 @@ screen_width = 1200
 screen_height = 800
 min_rad = 10
 max_rad = 50
+max_acc = 5
+max_vel = 43
 num_of_balls = 10
 score = 0
 targets_left = 1 + num_of_balls
+finished = False
+have_friend_param = False
+name = ""
+code = ""
+leaders = []
+
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -42,35 +50,49 @@ star_surf = pygame.transform.scale(star_surf, (balls_pool[0][2] * 2, balls_pool[
 
 font_name = pygame.font.match_font("Arial")
 
+
 def obj_collis(i):
     """
     function checks collisions of balls with walls
-    1 components int
+    1 component: int
     :param i: number of ball
     :return: void
     """
+    global have_friend_param
     flag = 0
     if balls_pool[i][0] + balls_pool[i][2] > 9 * screen_width // 10:  # collision with right wall
-        balls_pool[i][4] *= -1
-        balls_pool[i][0] = 9 * screen_width // 10 - balls_pool[i][2]
+        if i == 0 and have_friend_param:
+            balls_pool[0][0] = screen_width // 10 + balls_pool[0][2]
+        else:
+            balls_pool[i][4] *= -1
+            balls_pool[i][0] = 9 * screen_width // 10 - balls_pool[i][2]
         flag = 1
 
     if balls_pool[i][0] - balls_pool[i][2] < screen_width // 10:  # collision with left wall
-        balls_pool[i][4] *= -1
-        balls_pool[i][0] = screen_width // 10 + balls_pool[i][2]
+        if i == 0 and have_friend_param:
+            balls_pool[0][0] = 9 * screen_width // 10 - balls_pool[0][2]
+        else:
+            balls_pool[i][4] *= -1
+            balls_pool[i][0] = screen_width // 10 + balls_pool[i][2]
         flag = 1
 
     if balls_pool[i][1] + balls_pool[i][2] > 9 * screen_height // 10:  # collision with top wall
-        balls_pool[i][5] *= -1
-        balls_pool[i][1] = 9 * screen_height // 10 - balls_pool[i][2]
+        if i == 0 and have_friend_param:
+            balls_pool[0][1] = screen_height // 10 + balls_pool[0][2]
+        else:
+            balls_pool[i][5] *= -1
+            balls_pool[i][1] = 9 * screen_height // 10 - balls_pool[i][2]
         flag = 1
 
     if balls_pool[i][1] - balls_pool[i][2] < screen_height // 10:  # collision with bottom wall
-        balls_pool[i][5] *= -1
-        balls_pool[i][1] = screen_height // 10 + balls_pool[i][2]
+        if i == 0 and have_friend_param:
+            balls_pool[0][1] = 9 * screen_height // 10 - balls_pool[0][2]
+        else:
+            balls_pool[i][5] *= -1
+            balls_pool[i][1] = screen_height // 10 + balls_pool[i][2]
         flag = 1
-
-    if i == 0 and flag == 1:  # if ball is star, after collision it changes it's speed and acceleration
+    # if ball is star and you haven't a friend, after collision it changes it's speed and acceleration
+    if i == 0 and flag == 1 and not have_friend_param:
         balls_pool[0][7] = (1 - 2 * randint(0, 1)) * randint(0, 5)
         balls_pool[0][8] = (1 - 2 * randint(0, 1)) * randint(0, 5)
         if abs(balls_pool[0][4]) > 42:
@@ -82,7 +104,7 @@ def obj_collis(i):
 def draw_balls():
     """
     function draws balls
-    0 components
+    0 component:
     :param: none
     :return: void
     """
@@ -91,7 +113,6 @@ def draw_balls():
             continue
         balls_pool[i][0] += balls_pool[i][4]
         balls_pool[i][1] += balls_pool[i][5]
-
         obj_collis(i)
         circle(screen, balls_pool[i][3], (balls_pool[i][0], balls_pool[i][1]), balls_pool[i][2])
 
@@ -99,11 +120,11 @@ def draw_balls():
 def draw_star():
     """
     function draws star
-    0 components
+    0 component:
     :param: none
     :return: void
     """
-    global star_surf
+    global star_surf, max_vel
     if balls_pool[0][6] <= 0:
         return
     balls_pool[0][4] += balls_pool[0][7]
@@ -120,21 +141,21 @@ def draw_star():
 def click(event):
     """
     function handles mouse clicks
-    1 component pygame.MOUSEBUTTONDOWN
+    1 component: pygame.MOUSEBUTTONDOWN
     :param event: mouse click
     :return: void
     """
-    global score, targets_left
+    global score, targets_left, have_friend_param
     flag = 0
     mult = event.button
     for i in range(num_of_balls + 1):
         if balls_pool[i][6] > 0 and (event.pos[0] - balls_pool[i][0]) ** 2 + (event.pos[1] - balls_pool[i][1]) ** 2 <= \
                 balls_pool[i][2] ** 2:
             if i == 0:
-                score += mult * max_rad
+                score += mult * max_rad * (1 + have_friend_param)
                 screen.fill(YELLOW)
             else:
-                score += mult * (max_rad + min_rad - balls_pool[i][2])
+                score += mult * (max_rad + min_rad - balls_pool[i][2]) * (1 + have_friend_param)
             balls_pool[i][6] -= 1 * mult
             if balls_pool[i][6] <= 0:
                 targets_left -= 1
@@ -147,7 +168,7 @@ def click(event):
 def draw_text(screen, text, size, x, y):
     """
     function draws text bar with midtop coordinates x,y
-    5 components pygame.Surface, str, int, int, int
+    5 components: pygame.Surface, str, int, int, int
     :param screen: surface on which text bar is drawn
     :param text: what is written on text bar
     :param size: font size
@@ -163,119 +184,149 @@ def draw_text(screen, text, size, x, y):
     pygame.display.update()
 
 
-clock = pygame.time.Clock()
+def frame_quit(num_of_frame, event):
+    """
+    function processes quits from windows
+    2 components: int, pygame.event
+    :param num_of_frame: number of window
+    :param event: event from pygame.event.get()
+    :return: void
+    """
+    global targets_left
+    return (event.type == pygame.QUIT) or \
+           (num_of_frame == 1 and targets_left <= 0) or \
+           (num_of_frame == 0 and event.type == pygame.KEYDOWN and
+            (event.key == pygame.K_KP_ENTER or event.key == pygame.K_RETURN)) or \
+           ((num_of_frame == 2 or num_of_frame == 3) and (event.type == pygame.MOUSEBUTTONDOWN or
+            (event.type == pygame.KEYDOWN and (event.key == pygame.K_KP_ENTER or event.key == pygame.K_RETURN))))
 
-finished1 = False
-finished2 = False
-finished3 = False
-finished4 = False
-name = ""
-code = ""
+
+def leaderboard_changing():
+    """
+    function opens Leaderboard.txt and adds new player
+    0 component:
+    :return: void
+    """
+    inp = open('Leaderboard.txt', 'r')
+    global leaders, name, score
+    leaderboard = inp.readlines()
+    if leaderboard != ['\n']:
+        for i in leaderboard:
+            leaders.append((int(i.split(" ", 1)[0]), i.split(" ", 1)[1]))
+    inp.close()
+    for i in leaders:
+        if score >= i[0]:
+            leaders.insert(leaders.index(i), (score, name + "\n"))
+            break
+
+    if leaders == [] or score < leaders[-1][0]:
+        leaders.append((score, name + "\n"))
+
+    inp = open('Leaderboard.txt', 'w')
+    for i in leaders:
+        inp.write(str(i[0]) + " " + i[1])
+    inp.close()
 
 
-while not finished1:  # first window, enter your name
-    clock.tick(FPS)
-    screen.fill(BLACK)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished1 = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_BACKSPACE:
-                name = name[:-1]
-            elif event.key == pygame.K_KP_ENTER or event.key == pygame.K_RETURN:
-                finished1 = True
-            else:
-                name += event.unicode
+def before_events(num_of_frame, screen):
+    """
+    function makes stuff that should have done before events processing
+    2 components: int, pygame.screen
+    :param num_of_frame: number of window
+    :param screen: screen on which all stuff are drawn
+    :return: void
+    """
+    global name, score, leaders, screen_width, screen_height, have_friend_param, balls_pool
+    if num_of_frame == 0:
         draw_text(screen, "Enter your name:", screen_height // 10, screen_width // 2, 3 * screen_height // 10)
         draw_text(screen, name, screen_height // 10, screen_width // 2, 4 * screen_height // 10)
-        pygame.display.update()
+    elif num_of_frame == 1:
+        rect(screen, WHITE, (screen_width // 10, screen_height // 10, 8 * screen_width // 10, 8 * screen_height // 10), 3)
+        draw_balls()
+        draw_star()
+        draw_text(screen, "Score: " + str(score), screen_height // 10, screen_width // 2, 0)
+        keys = pygame.key.get_pressed()
+        if have_friend_param:
+            balls_pool[0][5] += keys[pygame.K_DOWN] and balls_pool[0][5] < max_vel
+            balls_pool[0][5] -= keys[pygame.K_UP] and balls_pool[0][5] > -max_vel
+            balls_pool[0][4] += keys[pygame.K_RIGHT] and balls_pool[0][4] < max_vel
+            balls_pool[0][4] -= keys[pygame.K_LEFT] and balls_pool[0][4] > -max_vel
+    elif num_of_frame == 2:
+        draw_text(screen, "Congrats, " + name + "!", screen_height // 10, screen_width // 2, screen_height // 9)
+        draw_text(screen, "Your score is " + str(score), screen_height // 10, screen_width // 2, 5 * screen_height // 9)
+        if score == -9999998:
+            draw_text(screen, "No, fuck you!", screen_height // 10, screen_width // 2, 4 * screen_height // 9)
+        elif score == 9999998:
+            draw_text(screen, "Cheater, booo", screen_height // 10, screen_width // 2, 4 * screen_height // 9)
+    else:
+        text_font = screen_height // max(10, len(leaders) + 1)
+        draw_text(screen, "Leaders:", screen_height // 10, screen_width // 2, 0)
+        for i in range(1, len(leaders) + 1):
+            draw_text(screen, (str(leaders[i - 1][0]) + " " + leaders[i - 1][1]).rstrip(), text_font, screen_width // 2,
+                      i * screen_height // (len(leaders) + 2) + screen_height // 10)
 
 
-while not finished2 and targets_left:  # second window, the game itself
-    clock.tick(FPS)
-
-    screen.fill(BLACK)
-    rect(screen, WHITE, (screen_width // 10, screen_height // 10, 8 * screen_width // 10, 8 * screen_height // 10), 3)
-    draw_balls()
-    draw_star()
-    draw_text(screen, "Score: " + str(score), screen_height // 10, screen_width // 2, 0)
-    pygame.display.update()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished2 = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+def events(num_of_frame, event):
+    """
+    function processes event from keyboard and mouse
+    2 components: int, pygame.event
+    :param num_of_frame: number of window
+    :param event: event from pygame.event.get()
+    :return: void
+    """
+    global name, score, code, balls_pool, targets_left, finished, have_friend_param, FPS
+    if num_of_frame == 0:  # first window, enter your name
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSPACE:
+                name = name[:-1]
+            else:
+                name += event.unicode
+                name = name.rstrip()
+    elif num_of_frame == 1:  # second window, the game itself
+        if event.type == pygame.MOUSEBUTTONDOWN:
             click(event)
             draw_text(screen, "Score: " + str(score), screen_height // 10, screen_width // 2, 0)
         elif event.type == pygame.KEYDOWN:
             code += event.unicode
             if "i want to win" in code:
                 score = 9999998
-                finished2 = True
+                finished = True
+                leaderboard_changing()
+                FPS //= 10
             elif "fuck you" in code:
                 score = -9999998
-                finished2 = True
+                finished = True
+                leaderboard_changing()
+                FPS //= 10
             elif "kill star" in code:
                 if balls_pool[0][6]:
                     balls_pool[0][6] = 0
                     targets_left -= 1
+            elif "i have a friend" in code:
+                have_friend_param = True
+                balls_pool[0][7] = 0
+                balls_pool[0][8] = 0
             print(code)
+    # third and last windows do nothing except waiting for closing
 
 
-while not finished3:  # third window, total
-    clock.tick(FPS // 5)
-    screen.fill(BLACK)
-    draw_text(screen, "Congrats, " + name + "!", screen_height // 10, screen_width // 2, screen_height // 9)
-    draw_text(screen, "Your score is " + str(score), screen_height // 10, screen_width // 2, 5 * screen_height // 9)
-    if score == -9999998:
-        draw_text(screen, "No, fuck you!", screen_height // 10, screen_width // 2, 4 * screen_height // 9)
-    if score == 9999998:
-        draw_text(screen, "Cheater, booo", screen_height // 10, screen_width // 2, 4 * screen_height // 9)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished3 = True
-        if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-            if event.type == pygame.MOUSEBUTTONDOWN or event.key == pygame.K_KP_ENTER or event.key == pygame.K_RETURN:
-                finished3 = True
+clock = pygame.time.Clock()
 
 
-# updating leaderboard
-inp = open('Leaderboard.txt', 'r')
-leaders = []
-leaderboard = inp.readlines()
-if leaderboard != ['\n']:
-    for i in leaderboard:
-        leaders.append((int(i.split(" ", 1)[0]), i.split(" ", 1)[1]))
-inp.close()
-for i in leaders:
-    if score >= i[0]:
-        leaders.insert(leaders.index(i), (score, name + "\n"))
-        break
-
-if leaders == [] or score < leaders[-1][0]:
-    leaders.append((score, name + "\n"))
-
-inp = open('Leaderboard.txt', 'w')
-for i in leaders:
-    inp.write(str(i[0]) + " " + i[1])
-inp.close()
-
-
-while not finished4:  # last window, leaderboard
-    clock.tick(FPS // 5)
-    screen.fill(BLACK)
-    text_font = screen_height // max(10, len(leaders) + 1)
-    draw_text(screen, "Leaders:", screen_height // 10, screen_width // 2, 0)
-    for i in range(1, len(leaders) + 1):
-        draw_text(screen, (str(leaders[i - 1][0]) + " " + leaders[i - 1][1]).rstrip(), text_font, screen_width // 2,
-                  i * screen_height // (len(leaders) + 2) + screen_height // 10)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished4 = True
-        if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-            if event.type == pygame.MOUSEBUTTONDOWN or event.key == pygame.K_KP_ENTER or event.key == pygame.K_RETURN:
-                finished4 = True
+for i in range(4):  # main body
+    finished = False
+    while not finished:
+        clock.tick(FPS)
+        screen.fill(BLACK)
+        before_events(i, screen)
+        for event in pygame.event.get():
+            if frame_quit(i, event):
+                finished = True
+                if i == 1:
+                    leaderboard_changing()
+                    FPS //= 10
+            events(i, event)
 
 
 pygame.quit()
+
